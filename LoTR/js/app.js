4d663e827a7baaca7a0aa618191d0fdcaa4fd5ee
Artiom Manokhin <amanokhin22@ukr.net>
2023-08-14 19:49:58 +0300

    Second commit. BG + video, bootstrap

--- LoTR/js/app.js
+++ LoTR/js/app.js
@@ -1,9 +1,22 @@
 window.addEventListener('scroll', e => {
     document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
-})
+});
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-ScrollSmoother.create ({
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+ScrollSmoother.create({
     wrapper: '.wrapper',
     content: '.content'
-})
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(data => {
+            const apiDataElement = document.getElementById('api-data');
+
+            apiDataElement.innerHTML = `<h2>API Data</h2><p>${JSON.stringify(data.title)}</p>`;
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+});
