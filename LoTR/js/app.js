fad6fa89150edf434001ae8090925dab611c3831
Artiom Manokhin <amanokhin22@ukr.net>
2023-08-15 21:26:21 +0300

    Third commit. BG and video + another one, bootstrap. Many interesting things with css

--- LoTR/js/app.js
+++ LoTR/js/app.js
@@ -20,3 +20,39 @@ window.addEventListener('DOMContentLoaded', () => {
             console.error('Error fetching data:', error);
         });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const showSlider = new Swiper('.showcase-carousel', {
+        loop: true,
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+            576: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 1,
+            },
+            992: {
+                slidesPerView: 1,
+            },
+            1200: {
+                slidesPerView: 3,
+            },
+        },
+        slidesPerView: 3,
+
+        speed: 1800,
+        centeredSlides: true,
+        autoplay: {
+            delay: 5000,
+        },
+        navigation: {
+            nextEl: '.showcase-navigation__next',
+            prevEl: '.showcase-navigation__prev'
+        }
+    })
+
+    document.querySelector('video').playbackRate = 2
+})
